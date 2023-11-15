4761c7500f879bb4804927b8e15adf8179bd0da4
Jackie <Jacqueline.moss23@gmail.com>
2023-11-14 19:28:54 -0600

    Frontend page built, javascript started, css layout done
    Okay so in this project the browser webpage was built. The JavaScript was also begun in the java script the names and ideas of the flavors was made to match what the HTML page had for consistency.  The result will be aimed to be displayed in the results box so the user can see what the webpage reads their order as.

---
+++ index.js
@@ -0,0 +1,9 @@
+const froyoFlavors = [
+    { id: 1, name: "vanilla" },
+    { id: 2, name: "strawberry" },
+    { id: 3, name: "coffee" },
+    { id: 4, name: "mudpie" },
+    { id: 5, name: "blueberry" },
+    { id: 6, name: "red velvet" },
+    { id: 7, name: "orange sherbert" }
+  ];
