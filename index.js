2d3243d237a69b25451d01bdbd4a5e1dfa973244
Jackie <Jacqueline.moss23@gmail.com>
2024-01-30 19:06:17 -0600

    Update java buttons
    updated java buttons for loops and to add quantites on the frond end

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const froyoFlavors = [
-    { id: 1, name: "vanilla" },
-    { id: 2, name: "strawberry" },
-    { id: 3, name: "coffee" },
-    { id: 4, name: "mudpie" },
-    { id: 5, name: "blueberry" },
-    { id: 6, name: "red velvet" },
-    { id: 7, name: "orange sherbert" }
-  ];
+let flavorQuantities = {
+  vanilla: 0,
+  strawberry: 0,
+  coffee: 0,
+  mudpie: 0,
+  blueberry: 0,
+  redVelvet: 0,
+  orangeSherbert: 0
+};
+
+function addFlavor(flavor) {
+  if (flavor in flavorQuantities) {
+      flavorQuantities[flavor]++;
+      document.getElementById(`quantity-${flavor}`).innerText = flavorQuantities[flavor];
+  }
+}
+
+function subtractFlavor(flavor) {
+  if (flavor in flavorQuantities && flavorQuantities[flavor] > 0) {
+      flavorQuantities[flavor]--;
+      document.getElementById(`quantity-${flavor}`).innerText = flavorQuantities[flavor];
+  }
+}
+
+function resetFlavors() {
+  for (let flavor in flavorQuantities) {
+      flavorQuantities[flavor] = 0;
+      document.getElementById(`quantity-${flavor}`).innerText = 0;
+  }
+}
+
